0f4a5be9a1508dff2676d2efd23d1ec94d51b5f3
zhouyuxi <1104622460@qq.com>
2022-08-24 23:28:51 +0800

    Draw a traingle

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -24,19 +24,36 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* glewInit */
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Error" << std::endl;
     }
-
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+
+    /*  */
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.0f,  0.5f
+    };
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);  //返回这个缓冲区的buffer id  ，使用1标识
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 

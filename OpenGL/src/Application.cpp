45886ee93bee1af07c4cd4779b2b1f4ec6088203
zhouyuxi <1104622460@qq.com>
2022-08-27 19:07:28 +0800

    Establish Renderer class

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -8,6 +8,7 @@
 
 #include "Renderer.h"
 
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexBuffer.h"
 #include "VertexArray.h"
@@ -73,7 +74,7 @@ int main(void)
     ib.Unbind();
     shader.Unbind();
 
-   
+    Renderer renderer;
 
     float r = 0.0f;
     float increment = 0.05f;
@@ -81,15 +82,15 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        renderer.Clear();
 
         shader.Bind();
         shader.SetUniform4f("u_Color", r, 0.2f, 0.35f, 1.0f);
 
-        va.Bind();
-        ib.Bind();
+       
 
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        renderer.Draw(va, ib, shader);
+        
         
         if (r > 1.0f)
             increment = -0.05f;
